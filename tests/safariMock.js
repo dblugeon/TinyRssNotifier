e98f58a835ed0a2b18a775bc716432fc98cdd55d
dblugeon <d@blugeon.com>
2010-07-02 16:21:41 +0200

    adding simulate extension.baseURI for the safariMock

--- tests/safariMock.js
+++ tests/safariMock.js
@@ -1,6 +1,7 @@
 var safari = new function(){
     this.extension = new function(){
         this.toolbarItems = new Array();
+        this.baseURI = document.baseURI.substr(0, document.baseURI.lastIndexOf("/"))+"/";
     };
     this.addMockToolbarItem = function(toolbarItem){
         this.extension.toolbarItems.push(toolbarItem);
@@ -18,5 +19,5 @@ var safari = new function(){
 function MockSafariToolbarItem(identifier, badge, image){
     this.identifier = "org.dbn.tinyrssnotifier-M2O3C5K "+ identifier;
     this.badge = badge == null ? 0 : badge;
-    this.image = image == null ? "mock.png" : image;
+    this.image = image == null ? safari.extension.basURI+"mock.png" : image;
 }
